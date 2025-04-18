9c27d8c10548673975a516ea824140a84fd9850e
HuuThang <huuthangblue0512@gmail.com>
2025-04-18 20:27:12 +0700

    create-basic-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useColorScheme } from "@mui/material/styles"
 import Select from "@mui/material/Select"
-import { Box, FormControl, InputLabel, MenuItem } from "@mui/material"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
 import LightModeIcon from "@mui/icons-material/LightMode"
 import DarkModeIcon from "@mui/icons-material/DarkMode"
 import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest"
@@ -47,13 +51,43 @@ function ModeSelect() {
 
 function App() {
   return (
-    <>
-      <div style={{ padding: "20px" }}>
-        <br />
+    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+      <Box
+        sx={{
+          backgroundColor: "primary.light",
+          width: "100%",
+          height: (theme) => theme.trello.appBarHeight,
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
         <ModeSelect />
-        <br />
-      </div>
-    </>
+      </Box>
+
+      <Box
+        sx={{
+          backgroundColor: "primary.dark",
+          width: "100%",
+          height: (theme) => theme.trello.boardBarHeight,
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        Board Bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          width: "100%",
+          height: (theme) =>
+            `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
