aaf85867afdbd700f8f76fab0e04de529cfe9b41
Huu Thang <huuthangblue0512@gmail.com>
2025-07-01 23:09:14 +0700

    fix bug card to empty col

--- index.py
+++
@@ -1,8 +0,0 @@
-def sum(a, b):
-    return a + b
-
-
-def sum_three(a, b, c):
-    return a + b + c
-
-print(sum_three(1, 2, 3))
--- src/apis/index.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-import { API_ROOT } from "~/utils/constants"
-export const fetchBoardDetailsAPI = async (boardId) => {
-  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
-  // axios trả về dữ liệu qua property của nó là data
-  return response.data
-}
